6873cb6bdaafe1f99b31381044d710dcaae16ef4
weap0n3 <weap0n1823@gmail.com>
2025-07-07 10:05:03 +0100

    Add project files.

---
+++ SmartClient.Gui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartClient.Gui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SmartClient.Gui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SmartClient.Gui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
