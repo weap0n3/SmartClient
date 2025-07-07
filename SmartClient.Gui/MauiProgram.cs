3448df9e7c52acbc89b7e0e11d0fc68d49dff0fb
weap0n3 <weap0n1823@gmail.com>
2025-07-07 11:35:37 +0100

    added fonts

--- SmartClient.Gui/MauiProgram.cs
+++ SmartClient.Gui/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace SmartClient.Gui
 {
@@ -9,10 +10,15 @@ namespace SmartClient.Gui
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Raleway-VariableFont.ttf", "Raleway");
+                    fonts.AddFont("Inter-VariableFont.ttf", "Inter");
+                    fonts.AddFont("Raleway-Bold.ttf", "Raleway-Bold");
+                    fonts.AddFont("segoeuithis.ttf", "Segoe UI");
                 });
 
 #if DEBUG
