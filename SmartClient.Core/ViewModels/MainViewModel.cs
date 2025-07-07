a9cacde0c13529318d27b51f9eba52adc558c575
weap0n3 <weap0n1823@gmail.com>
2025-07-07 11:20:19 +0100

    add mainviewmodel

---
+++ SmartClient.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartClient.Core.ViewModels;
+
+public class MainViewModel
+{
+}
