491d1791474c70666e91c525be56e48b8f54bb5d
weap0n3 <weap0n1823@gmail.com>
2025-07-08 08:29:16 +0100

    create new Model

---
+++ SmartClient.Data/Models/Profile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartClient.Data.Models;
+
+public class Profile
+{
+    public string CCID { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Tele { get; set; }
+    public string Mail { get; set; }
+    public string Contact { get; set; }
+    public string Ip_Address { get; set; }
+    public string Port { get; set; }
+    public string Version { get; set; }
+}
+
