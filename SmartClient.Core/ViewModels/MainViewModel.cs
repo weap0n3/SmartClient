6eec7ab6566e040b6c1f9d06f38fae738ff61b60
weap0n3 <weap0n1823@gmail.com>
2025-07-08 08:29:45 +0100

    profiles loading ui

--- SmartClient.Core/ViewModels/MainViewModel.cs
+++ SmartClient.Core/ViewModels/MainViewModel.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SmartClient.Data.Models;
+using SmartClient.Data.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SmartClient.Core.ViewModels;
 
-public class MainViewModel
+public partial class MainViewModel: ObservableObject
 {
+    private readonly IMemory _memory;
+    public MainViewModel(IMemory memory)
+    {
+        this._memory = memory;
+    }
+
+
+    [ObservableProperty]
+    private ObservableCollection<Profile> _profiles;
+
+    [RelayCommand]
+    private async void Load()
+    {
+        Profiles = new ObservableCollection<Profile>(await _memory.GetProfiles());
+    }
 }
