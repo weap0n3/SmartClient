ae18af061719983d4891c7ff472db777c7fa4ce9
weap0n3 <weap0n1823@gmail.com>
2025-07-11 09:28:11 +0100

    add filters feature

--- SmartClient.Core/ViewModels/MainViewModel.cs
+++ SmartClient.Core/ViewModels/MainViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SmartClient.Data.Models;
+using SmartClient.Data.Enums;
 using SmartClient.Data.Services;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace SmartClient.Core.ViewModels;
 
@@ -17,6 +19,9 @@ public partial class MainViewModel: ObservableObject
     [ObservableProperty]
     private ObservableCollection<Profile> _filteredProfiles;
 
+    [ObservableProperty]
+    private ObservableCollection<Profile> _allProfiles;
+
     [NotifyCanExecuteChangedFor(nameof(StartAppCommand))]
     [ObservableProperty]
     public Profile _selectedProfile;
@@ -24,27 +29,36 @@ public partial class MainViewModel: ObservableObject
 
     private int index = -1;
 
+    private FilterMode _currentFilterMode = FilterMode.None;
+
     [ObservableProperty]
     public string _searchQuery;
 
     partial void OnSearchQueryChanged(string value)
     {
-        var allprofiles = _memory.LoadCachedProfiles();
+        var noneSearch = _memory.LoadCachedProfiles();
 
         if (SearchQuery == null || SearchQuery == string.Empty)
         {
-            FilteredProfiles = new ObservableCollection<Profile>(allprofiles); 
+            FilteredProfiles = new ObservableCollection<Profile>(noneSearch);
+            NameFilterMode = FilterMode.None;
+            OrtFilterMode = FilterMode.None;
+            PersonFilterMode = FilterMode.None;
         }
         else
         {
             FilteredProfiles = new ObservableCollection<Profile>
             (
-                allprofiles.Where
+                AllProfiles.Where
                 (
                     p => p.Name.ToLower().Contains(SearchQuery.ToLower()) ||
                     p.Contact.ToLower().Contains(SearchQuery.ToLower()) ||
                     p.City.ToLower().Contains(SearchQuery.ToLower()) ||
-                    p.Mail.ToLower().Contains(SearchQuery.ToLower())
+                    p.Mail.ToLower().Contains(SearchQuery.ToLower()) ||
+                    p.Tele.ToLower().Contains(SearchQuery.ToLower()) ||
+                    p.Ip_Address.ToLower().Contains(SearchQuery.ToLower()) ||
+                    p.Port.ToLower().Contains(SearchQuery.ToLower()) ||
+                    p.Version.ToLower().Contains(SearchQuery.ToLower()) 
                 )
             );
         }
@@ -66,22 +80,86 @@ public partial class MainViewModel: ObservableObject
     [RelayCommand]
     private async void Load()
     {
+        _memory.DownloadLibs();
         var cached = _memory.LoadCachedProfiles();
-        FilteredProfiles = new ObservableCollection<Profile>(cached);
+        AllProfiles = new ObservableCollection<Profile>(cached);
+        FilteredProfiles = AllProfiles;
 
         await _memory.LoadFromApiProfiles();
         var updated = _memory.LoadCachedProfiles();
 
         if (!cached.Select(p => p.CCID).SequenceEqual(updated.Select(p => p.CCID)))
         {
-            FilteredProfiles = new ObservableCollection<Profile>(updated);
+            AllProfiles = new ObservableCollection<Profile>(updated);
             System.Diagnostics.Debug.WriteLine("Update");
         }
+
+        FilteredProfiles = AllProfiles;
     }
 
     [RelayCommand(CanExecute = nameof(CanStart))]
     private void StartApp()
     {
         _memory.StartCapHotel(SelectedProfile);
+    }
+    private readonly Dictionary<string, FilterMode> _filterModes = new()
+    {
+        { "Name", FilterMode.None },
+        { "Ort", FilterMode.None },
+        { "Person", FilterMode.None }
+    };
+
+    [ObservableProperty]
+    public FilterMode _nameFilterMode;
+
+    [ObservableProperty]
+    public FilterMode _ortFilterMode;
+
+    [ObservableProperty]
+    public FilterMode _personFilterMode;
+
+    [RelayCommand]
+    private void ToggleFilter(string filterQuery)
+    {
+        var noneFilter = _memory.LoadCachedProfiles();
+        _filterModes[filterQuery] = _filterModes[filterQuery] switch
+        {
+            FilterMode.None => FilterMode.Descending,
+            FilterMode.Descending => FilterMode.Ascending,
+            FilterMode.Ascending => FilterMode.None ,
+            _ => FilterMode.None
+        };
+        switch (filterQuery)
+        {
+            case "Name":
+                AllProfiles = _filterModes["Name"] switch
+                {
+                    FilterMode.Ascending => new ObservableCollection<Profile>(FilteredProfiles.OrderBy(p => p.Name)),
+                    FilterMode.Descending => new ObservableCollection<Profile>(FilteredProfiles.OrderByDescending(p => p.Name)),
+                    FilterMode.None => new ObservableCollection<Profile>(noneFilter)
+                };
+                break;
+            case "Ort":
+                AllProfiles = _filterModes["Ort"] switch
+                {
+                    FilterMode.Ascending => new ObservableCollection<Profile>(FilteredProfiles.OrderBy(p => p.City)),
+                    FilterMode.Descending => new ObservableCollection<Profile>(FilteredProfiles.OrderByDescending(p => p.City)),
+                    FilterMode.None => new ObservableCollection<Profile>(noneFilter)
+                };
+                break;
+            case "Person":
+                AllProfiles = _filterModes["Person"] switch
+                {
+                    FilterMode.Ascending => new ObservableCollection<Profile>(FilteredProfiles.OrderBy(p => p.Contact)),
+                    FilterMode.Descending => new ObservableCollection<Profile>(FilteredProfiles.OrderByDescending(p => p.Contact)),
+                    FilterMode.None => new ObservableCollection<Profile>(noneFilter)
+                };
+                break;
+        }
+
+        NameFilterMode = _filterModes["Name"];
+        OrtFilterMode = _filterModes["Ort"];
+        PersonFilterMode = _filterModes["Person"];
+        FilteredProfiles = AllProfiles;
     }
 }
---
+++ SmartClient.Data/Enums/FilterMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartClient.Data.Enums;
+
+public enum FilterMode
+{
+    None,
+    Ascending,
+    Descending
+}
