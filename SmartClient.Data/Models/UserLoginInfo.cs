e3c905dc17833413aee87f4f0abf2a3484b9ec58
weap0n3 <weap0n1823@gmail.com>
2025-07-14 16:19:06 +0100

    added new Model

---
+++ SmartClient.Data/Models/UserLoginInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartClient.Data.Models;
+
+public class UserLoginInfo
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+    public UserLoginInfo(string username,string password, bool rememberme)
+    {
+        this.Username = username;
+        this.Password = password;
+        this.RememberMe = rememberme;
+    }
+}
