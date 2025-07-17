6d2067c3b1557eabe8c29e6cfa770a69bde5062f
weap0n3 <weap0n1823@gmail.com>
2025-07-17 16:27:44 +0100

    created ProfileMapper

---
+++ SmartClient.Data/Services/ProfileMapper.cs
@@ -0,0 +1,32 @@
+﻿using SmartClient.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartClient.Data.Services;
+
+public static class ProfileMapper
+{
+    private static ProfileUI ToUI(Profile profile)
+    {
+        return new ProfileUI(profile);
+    }
+
+    public static Profile ToData(ProfileUI profileUI)
+    {
+        return profileUI.profile;
+    }
+
+    public static List<ProfileUI> ListToUI(List<Profile> profiles)
+    {
+        List<ProfileUI> uiProfiles = new List<ProfileUI>();
+        foreach(Profile p in profiles)
+        {
+            uiProfiles.Add(ToUI(p));
+        }
+        return uiProfiles;
+    }
+}
+
